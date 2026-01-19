8c481ef91fad94273b0cca6b93d370a53fd3dea3
Felipe Uribe <wfpaisa@gmail.com>
2026-01-18 19:56:26 -0500

    demo-n2

--- src/App.tsx
+++ src/App.tsx
@@ -1,142 +1,56 @@
-import React, { useMemo, useState } from "react";
-import { Input, Tree } from "antd";
-import type { TreeDataNode } from "antd";
+import "./App.css";
+import "react-complex-tree/lib/style-modern.css";
+import {
+  UncontrolledTreeEnvironment,
+  Tree,
+  StaticTreeDataProvider,
+} from "react-complex-tree";
 
-const { Search } = Input;
-
-const x = 3;
-const y = 2;
-const z = 1;
-const defaultData: TreeDataNode[] = [];
-
-const generateData = (
-  _level: number,
-  _preKey?: React.Key,
-  _tns?: TreeDataNode[],
-) => {
-  const preKey = _preKey || "0";
-  const tns = _tns || defaultData;
-
-  const children: React.Key[] = [];
-  for (let i = 0; i < x; i++) {
-    const key = `${preKey}-${i}`;
-    tns.push({ title: key, key });
-    if (i < y) {
-      children.push(key);
-    }
-  }
-  if (_level < 0) {
-    return tns;
-  }
-  const level = _level - 1;
-  children.forEach((key, index) => {
-    tns[index].children = [];
-    return generateData(level, key, tns[index].children);
-  });
-};
-generateData(z);
-
-const dataList: { key: React.Key; title: string }[] = [];
-const generateList = (data: TreeDataNode[]) => {
-  for (let i = 0; i < data.length; i++) {
-    const node = data[i];
-    const { key } = node;
-    dataList.push({ key, title: key as string });
-    if (node.children) {
-      generateList(node.children);
-    }
-  }
-};
-generateList(defaultData);
-
-const getParentKey = (key: React.Key, tree: TreeDataNode[]): React.Key => {
-  let parentKey: React.Key;
-  for (let i = 0; i < tree.length; i++) {
-    const node = tree[i];
-    if (node.children) {
-      if (node.children.some((item) => item.key === key)) {
-        parentKey = node.key;
-      } else if (getParentKey(key, node.children)) {
-        parentKey = getParentKey(key, node.children);
-      }
-    }
-  }
-  return parentKey!;
-};
-
-const App: React.FC = () => {
-  const [expandedKeys, setExpandedKeys] = useState<React.Key[]>([]);
-  const [searchValue, setSearchValue] = useState("");
-  const [autoExpandParent, setAutoExpandParent] = useState(true);
-
-  const onExpand = (newExpandedKeys: React.Key[]) => {
-    setExpandedKeys(newExpandedKeys);
-    setAutoExpandParent(false);
+function App() {
+  const items = {
+    root: {
+      index: "root",
+      isFolder: true,
+      children: ["child1", "child2"],
+      data: "Root item",
+    },
+    child1: {
+      index: "child1",
+      children: [],
+      data: "Child item 1",
+    },
+    child2: {
+      index: "child2",
+      isFolder: true,
+      children: ["child3"],
+      data: "Child item 2",
+    },
+    child3: {
+      index: "child3",
+      children: [],
+      data: "Child item 3",
+    },
   };
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    const newExpandedKeys = dataList
-      .map((item) => {
-        if (item.title.includes(value)) {
-          return getParentKey(item.key, defaultData);
-        }
-        return null;
-      })
-      .filter(
-        (item, i, self): item is React.Key =>
-          !!(item && self.indexOf(item) === i),
-      );
-    setExpandedKeys(newExpandedKeys);
-    setSearchValue(value);
-    setAutoExpandParent(true);
-  };
-
-  const treeData = useMemo(() => {
-    const loop = (data: TreeDataNode[]): TreeDataNode[] =>
-      data.map((item) => {
-        const strTitle = item.title as string;
-        const index = strTitle.indexOf(searchValue);
-        const beforeStr = strTitle.substring(0, index);
-        const afterStr = strTitle.slice(index + searchValue.length);
-        const title =
-          index > -1 ? (
-            <span key={item.key}>
-              {beforeStr}
-              <span className="site-tree-search-value">{searchValue}</span>
-              {afterStr}
-            </span>
-          ) : (
-            <span key={item.key}>{strTitle}</span>
-          );
-        if (item.children) {
-          return { title, key: item.key, children: loop(item.children) };
-        }
-
-        return {
-          title,
-          key: item.key,
-        };
-      });
-
-    return loop(defaultData);
-  }, [searchValue]);
+  const dataProvider = new StaticTreeDataProvider(items, (item, newName) => ({
+    ...item,
+    data: newName,
+  }));
 
   return (
-    <div>
-      <Search
-        style={{ marginBottom: 8 }}
-        placeholder="Filtrar"
-        onChange={onChange}
-      />
-      <Tree
-        onExpand={onExpand}
-        expandedKeys={expandedKeys}
-        autoExpandParent={autoExpandParent}
-        treeData={treeData}
-      />
-    </div>
+    <>
+      <UncontrolledTreeEnvironment
+        dataProvider={dataProvider}
+        getItemTitle={(item) => item.data}
+        viewState={{}}
+        canDragAndDrop={true}
+        canDropOnFolder={true}
+        canReorderItems={true}
+      >
+        <Tree treeId="tree-2" rootItem="root" treeLabel="Tree Example" />
+      </UncontrolledTreeEnvironment>
+    </>
   );
-};
+}
 
 export default App;
