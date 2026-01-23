9d39e120831e74e8112b7b8bf837abbeeab927bd
Felipe Uribe <wfpaisa@gmail.com>
2026-01-23 18:23:34 -0500

    refactor: unify string quotation style and improve code formatting in extract_names and translate_parents scripts

--- extract_names.js
+++ extract_names.js
@@ -1,19 +1,21 @@
-import fs from 'fs';
+import fs from "fs";
 
-const data = JSON.parse(fs.readFileSync('./server/data/bookmarks.json', 'utf8'));
+const data = JSON.parse(
+  fs.readFileSync("./server/data/bookmarks.json", "utf8"),
+);
 
-function findNamesWithChildren(obj, path = '') {
+function findNamesWithChildren(obj, path = "") {
   const names = [];
   if (obj.children && obj.children.length > 0) {
     names.push({ name: obj.name, path });
     obj.children.forEach((child, index) => {
-      names.push(...findNamesWithChildren(child, path + '/' + obj.name));
+      names.push(...findNamesWithChildren(child, path + "/" + obj.name));
     });
   }
   return names;
 }
 
-const namesWithChildren = data.flatMap(item => findNamesWithChildren(item));
+const namesWithChildren = data.flatMap((item) => findNamesWithChildren(item));
 
-console.log('Names with children:');
-namesWithChildren.forEach(item => console.log(item.name));
+console.log("Names with children:");
+namesWithChildren.forEach((item) => console.log(item.name));
